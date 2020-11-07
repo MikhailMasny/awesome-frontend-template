f330d9c14ce5f029c89a37ba0914bedd5b1c9fc7
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-11-08 01:14:20 +0300

    feat: add webpack, sass and tsconfig

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import "./style.scss";
+
+const email =  document.getElementById("email") as HTMLInputElement;
+const name1 = document.getElementById("name") as HTMLInputElement;
+
+const xhr = new XMLHttpRequest();
+xhr.open("POST", "/api");
+xhr.setRequestHeader("Content-type", "application/json");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.send(JSON.stringify({ email, name1 }));
+
+xhr.onreadystatechange = () => {
+  if (xhr.readyState !== 4) {
+    return;
+  }
+  if (xhr.status !== 200) {
+    const response: Partial<{ errors: { name1: string; email: string } }> = JSON.parse(xhr.response);
+    const errors = document.getElementById("errors") as HTMLDivElement;
+    errors.innerHTML = response.errors ? (response.errors.email || response.errors.name1) : "Send error. Try again.";
+  } else {
+    const success = document.getElementById("success") as HTMLDivElement;
+    success.innerHTML = "Form sent!";
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  target: 'web',
+  entry: {
+    app: path.join(__dirname, 'src', 'index.ts')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 3000
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[contenthash].[ext]',
+            publicPath: 'images',
+            outputPath: 'images',
+            esModule: false
+          },
+        },
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist")
+        },
+      ],
+    }),
+  ],
+};
